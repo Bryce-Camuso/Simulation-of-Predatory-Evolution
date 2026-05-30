162634124b026baa6ff402b600422a3387702b53
unknown <camusobryce@gmail.com>
2026-05-30 15:50:45 -0400

    Map class compleate

--- classes/Map.py
+++ classes/Map.py
@@ -69,10 +69,30 @@ class Map:
 
 def tester():
     map1 = Map()
-    print(map1.get_map_list([(0,1),(5,5), (9,9)]))
 
-    map2 = Map()
-    print(map2.get_map_list([(0,1),(5,5), (9,9)]))
+    print('---------------------------------------------------------------------------------------------------------------------')
+
+    print('Map Size test')
+
+    if len(map1._map) == map1._MAPSCALE + 1:
+        print('Map Size X Scale: pass')
+    else:
+        print('Map Size X Scale: fail')
+
+    
+    testFlag = True
+    for i in map1._map:
+        if len(i) != map1._MAPSCALE + 1:
+            testFlag = False
+
+    if testFlag:
+        print('Map Size Y Scale: pass')
+    else:
+        print('Map Size Y Scale: fail')
+    
+    print('---------------------------------------------------------------------------------------------------------------------')
+    print('Map Tile Distribution test')
+
 
     plainCount = 0
     treeCount = 0
@@ -86,9 +106,70 @@ def tester():
 
             totalCount += 1
     
-    print('plain average: ' + str(plainCount / totalCount))
-    print('tree average: ' + str(treeCount / totalCount))
-    print('bush average: ' + str(bushCount / totalCount))
+    if round(plainCount / totalCount, 2) == 0.60:
+        print('Map Plain tile Distribution: pass')
+    else:
+        print('Map Plain tile Distribution: fail')
+
+    if round(treeCount / totalCount, 2) == 0.20:
+        print('Map Tree tile Distribution: pass')
+    else:
+        print('Map Tree tile Distribution: fail')
+
+    if round(bushCount / totalCount, 2) == 0.20:
+        print('Map Bush tile Distribution: pass')
+    else:
+        print('Map Bush tile Distribution: fail')
+
+    print('---------------------------------------------------------------------------------------------------------------------')
+    print('Map Getters test')
+
+    testSample = map1.get_x(9)
+    if testSample == map1._map[9]:
+        print('Map Get X: pass')
+    else:
+        print('Map Get X: fail')
+
+    testSample = map1.get_y(9)
+    exampleArray = []
+    for i in map1._map:
+        exampleArray.append(i[9])
+
+    if testSample == exampleArray:
+        print('Map Get Y: pass')
+    else:
+        print('Map Get Y: fail')
+
+    testPoints = [(9,1), (10,2), (100, 43), (14, 46), (57,32), (33, 88), (87, 93), (55, 55), (49, 69), (31, 80)]
+    testSample = map1.get_map_list(testPoints)
+    
+    testFlag = True
+    for i in range(len(testPoints)):
+        if testPoints[i] != testSample[i][0]:
+            testFlag = False
+        
+
+    if testFlag:
+        print('Map Get List: pass')
+    else:
+        print('Map Get List: fail')
+
+    print('---------------------------------------------------------------------------------------------------------------------')
+    print('Map Singleton test')
+    # has a 1.6935087808430286711036596724754e-5% = (1/3)^10 chance of working without being a singleton 
+    testPoints = [(9,1), (10,2), (100, 43), (14, 46), (57,32), (33, 88), (87, 93), (55, 55), (49, 69), (31, 80)]
+    testSample1 = map1.get_map_list(testPoints)
+
+    map2 = Map()
+    testSample2 = map2.get_map_list(testPoints)
+
+    if testSample1 == testSample2:
+        print('Map ingleton: pass')
+    else:
+        print('Map ingleton: fail')
+
+
+    
     
 
 if __name__ == '__main__':
